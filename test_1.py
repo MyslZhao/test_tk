589ebf95ce852113051a82f411f0e97d3da43dec
Kocmocer <pythos@163.com>
2024-06-04 10:14:51 +0800

    ok

--- test_1.py
+++ test_1.py
@@ -7,6 +7,6 @@ ltime:2024-6-4 10:10
 import tkinter as tk #导入Tk
 w=tk.Tk() #Tk对象"w"
 l=tk.Label(w,anchor="nw",bd=4,cursor="cross",text="Hello world!") #Label对象"l"
-b=tk.Button(w,command="l.pack()") #控制"l"的载入
+b=tk.Button(w,command="l.pack()",text="点一下试试") #控制"l"的载入
 b.pack() #载入"b"
 m.mainloop() #开启窗口
