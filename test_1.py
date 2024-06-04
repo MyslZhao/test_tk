2ba050079f4b71b8b812d869f7878dc97589bb83
enterusrname <159505988+enterusrname@users.noreply.github.com>
2024-06-04 10:04:57 +0800

    更新 test_1.py

--- test_1.py
+++ test_1.py
@@ -6,6 +6,6 @@ ltime:2024-6-3 23:12
 """
 import tkinter as tk #导入Tk
 m=tk.Tk() #Tk对象"m"
-m.mainloop() #开启窗口
 w=tk.Label(m,anchor="nw",bd=4,cursor="cross",text="Hello world!") #Label对象"w"
 w.pack() #载入"w"
+m.mainloop() #开启窗口
