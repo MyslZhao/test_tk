e109dddf75434631ac79bcda6c89ab5b238ae2f4
Kocmocer <pythos@163.com>
2024-06-06 07:58:57 +0800

    updata

--- test_1.py
+++ test_1.py
@@ -1,12 +1,19 @@
 """
 owner:enterusrname@github.com
-version:0.02a
+version:0.03a
 cTime:2024-6-3 23:12
-ltime:2024-6-4 10:10
+ltime:2024-6-6 07:46
 """
-import tkinter as tk #导入Tk
-w=tk.Tk() #Tk对象"w"
-l=tk.Label(w,anchor="nw",bd=4,cursor="cross",text="Hello world!") #Label对象"l"
-b=tk.Button(w,command="l.pack()",text="点一下试试") #控制"l"的载入
-b.pack() #载入"b"
-m.mainloop() #开启窗口
+import tkinter as tk
+w=tk.Tk()
+tk.Frame(w).pack()
+e=tk.Entry(fa,show="*")
+e.pack()
+def job():
+    l.pack()
+    w.title=e.get()
+    return 0
+
+l=tk.Label(fa,anchor="nw",bd=4,cursor="cross",text="Hello world!")
+tk.Button(w,command="job()",text="点一下试试").pack()
+m.mainloop()
