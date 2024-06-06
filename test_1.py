2dfe97e0f1d8e04fc4a36c3e469908e685609e1d
enterusrname <vselennaya@163.com>
2024-06-06 10:20:04 +0800

    ud

--- test_1.py
+++ test_1.py
@@ -1,8 +1,8 @@
 """
 owner:enterusrname@github.com
-version:0.03f
+version:0.03g
 cTime:2024-6-3 23:12
-ltime:2024-6-6 10:08
+ltime:2024-6-6 10:19
 """
 import tkinter as tk
 w=tk.Tk()
@@ -12,7 +12,7 @@ e=tk.Entry(fa,show="*",bd=10,bg="yellow")
 e.pack()
 def job():
     l.pack()
-    w.title=e.get()
+    w.title(e.get())
     return 0
 
 l=tk.Label(fa,anchor="nw",bd=4,cursor="cross",text="Hello world!")
