0da23498d5e5fa6e23c786f9d215df115e739d16
MyslZhao <vselennaya@163.com>
2025-06-17 10:52:07 +0800

    五子棋:(v0.1)

--- pygame/exercise_space.py
+++ pygame/exercise_space.py
@@ -4,13 +4,12 @@ import pygame as pg
 
 # 初始化Pygame
 pg.init()
-
 # 设置窗口大小
 size = (700, 500)
 screen = pg.display.set_mode(size)
 
 # 设置窗口标题
-pg.display.set_caption("PyGame")
+pg.display.set_caption("abcde")
 
 # 设置矩形位置和大小
 rect_x = 50
@@ -24,7 +23,8 @@ red = (255, 0, 0)
 # 游戏循环
 done = False
 
-
+screen.fill((255, 255, 255))
+pg.display.update()
 while not done:
     # 处理事件
     for event in pg.event.get():
@@ -32,7 +32,7 @@ while not done:
             done = True
 
     # 填充窗口颜色
-    screen.fill((255, 255, 255))
+    
 
     # 绘制矩形
     pg.draw.rect(screen, red, [rect_x, rect_y, rect_width, rect_height])
--- 五子棋/main.py
+++ 五子棋/main.py
@@ -1,2 +1,25 @@
 import pygame as pg
+# -*- coding: utf-8 -*-
+bit_loca=(554,532)
 
+pg.init() #pygame 初始化
+surface = pg.display.set_mode((652, 652))
+pg.display.set_caption("五子棋")  # 设置窗口标题为"五子棋"
+surface.fill((255, 255, 255))  # 设置背景颜色为白色
+
+pg.display.set_icon(pg.image.load("五子棋\\icon.jpg"))  # 设置窗口图标
+
+surface.blit(pg.image.load("五子棋\\background.jpg"), (0, 0))  # 绘制背景图(ps:找不到什么好图，只能先用围棋图代替)
+surface.blit(pg.image.load("五子棋\\chessboard.jpg"), (20, 20))  # 绘制棋盘
+pg.display.update()  # 更新显示
+
+
+
+
+#pygame 循环
+done = False
+
+while not done:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done = True
