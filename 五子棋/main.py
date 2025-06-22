42fb3edba0b805bbb88404ac4c4d3bf01375aeac
MyslZhao <vselennaya@163.com>
2025-06-22 11:48:51 +0800

    五子棋:(1.0)

--- 五子棋/main.py
+++ 五子棋/main.py
@@ -13,29 +13,29 @@ COLOR={0:None,1:'白方',2:'黑方'}  # 定义颜色字典
 COLOR_PIC={0:None,1:WHITE_PIC,2:BLACK_PIC}
 
 class board:
-    statue_board=[]
-    start_point=(47,49)
+    """
+    棋盘类
+    """
+    turn_disp_point=(20,628)
+    statue_board=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    start_point=(47,48)
     pix_size = (0,0)
     size = (15, 15)
-    x_dist = 0
-    y_dist = 0
+    x_dist = 39.88
+    y_dist = 38.1
     x_loca=[]
     y_loca=[]
-    bias = 12
+    bias = 5 # 用于修正玩家点击位置时的偏差，不宜过大
     player = []  # 玩家列表，存储两个玩家对象
     def __init__(self, size: tuple[int, int], player1: 'player', player2: 'player'):
         """
          棋盘对象初始化
         """
-        cache=[]
-        for i in range(15):
-            cache.append(0)
-        for j in range(15):
-            self.statue_board.append(cache)
-        del cache
+        for i in self.statue_board:
+            for j in range(15):
+                i.append(0)
+        print(self.statue_board)
         self.pix_size = size  # 设置棋盘像素大小
-        self.x_dist=int(self.pix_size[0] / (self.size[0]-1)) #计算棋格宽度
-        self.y_dist=int(self.pix_size[0] / (self.size[0]-1)) #计算棋格高度
         for i in range(15):
             # 位置序号
             self.x_loca.append(i)
@@ -44,8 +44,93 @@ class board:
         self.player.append(player2)  # 添加玩家2
         return None
     def check(self):
+        l=self.statue_board #为了简化名称，进行引用
+        # 横向五子检查
+        for i in l:
+            if len([k for k,x in enumerate(i) if x == 1]) < 5 and len([l for l,y in enumerate(i) if y == 2]) < 5:
+                print(len([k for k,x in enumerate(i) if x == 1]),len([l for l,y in enumerate(i) if y == 2]))
+                print(i)
+                continue
+            for j in range(10):
+                cache=i[j]*i[j+1]*i[j+2]*i[j+3]*i[j+4]
+                print(cache)
+                if cache == 1 or cache == 32:
+                    return True
+        # 纵向五子检查
+        for i in range(15):
+            cache=[]
+            for j in l:
+                cache.append(j[i])
+            if len([k for k,x in enumerate(cache) if x == 1]) < 5 and len([l for l,y in enumerate(cache) if y == 2]) < 5:
+                continue
+            for j in range(10):
+                if cache[j] == 0:
+                    continue
+                else:
+                    a=cache[j]*cache[j+1]*cache[j+2]*cache[j+3]*cache[j+4]
+                    if a == 1 or a == 32:
+                        return True
+        # 斜向五子检查
+        for i in range(15):
+            for j in range(15):
+                cache=0
+                if l[i][j] == 0:
+                    continue
+                elif i < 11 and j <11:
+                    cache=l[i][j]*l[i+1][j+1]*l[i+2][j+2]*l[i+3][j+3]*l[i+4][j+4]
+                elif i > 3 and j < 11:
+                    cache=l[i][j]*l[i-1][j+1]*l[i-2][j+2]*l[i-3][j+3]*l[i-4][j+4]
+                else:
+                    continue
+                if cache == 1 or cache == 32:
+                    return True
         return False
+
+
+    
+    def test_place_piece(self,player:'player',surface):
+        """
+        棋盘点阵可视化,
+        用于调试(不影响其他功能)
+        """
+        done = False
+        statue = []
+        # 具体可视化实现
+        for i in range(15):
+            for j in range(15):
+                pg.draw.rect(surface,
+                             (255,255,255),
+                             pg.Rect(i*self.x_dist+self.start_point[0]-self.bias,
+                                     j*self.y_dist+self.start_point[1]-self.bias,
+                                     2*self.bias,
+                                     2*self.bias))
+        pg.display.update()
+        # 下同self.place_piece
+        while not done:
+            pix_loca=list(player.place_piece())
+            if str(pix_loca[0]) == 'False':
+                done=True
+                statue=[False,False]
+                continue
+            for x in self.x_loca:
+                for y in self.y_loca:
+                    m=(x*self.x_dist-self.bias+self.start_point[0]-pix_loca[0])*(x*self.x_dist+self.bias+self.start_point[0]-pix_loca[0])
+                    n=(y*self.y_dist-self.bias+self.start_point[1]-pix_loca[1])*(y*self.y_dist+self.bias+self.start_point[1]-pix_loca[1])
+                    if m <= 0 and n <= 0 and self.statue_board[x][y] == 0:
+                        print(player.color)                            
+                        surface.blit(COLOR_PIC[player.color], 
+                                    (x*self.x_dist+self.start_point[0] - player.bias[player.color-1][0], 
+                                    y*self.y_dist+self.start_point[1] - player.bias[player.color-1][1]))
+                        pg.display.update()
+                        print(self.statue_board)
+                        done =True
+                        statue=[x,y]
+                        continue
+        return statue
     def place_piece(self,player:'player',surface):
+        """
+        对鼠标所点击的坐标进行判定
+        """
         done = False
         statue = []
         while not done:
@@ -56,43 +141,68 @@ class board:
                 continue
             for x in self.x_loca:
                 for y in self.y_loca:
-                    m=(x*self.x_dist-self.bias+self.start_point[0]-pix_loca[0])*(x+self.bias+self.start_point[0]-pix_loca[0])
-                    n=(y*self.y_dist-self.bias+self.start_point[1]-pix_loca[1])*(y+self.bias+self.start_point[1]-pix_loca[1])
-                    print(m,n)
-                    if m <= 0 and n <= 0:
-                        print(player.color)
+                    # 坐标判断原理:'a < m < b '<=> '(a-m)*(b-m) < 0'
+                    m=(x*self.x_dist-self.bias+self.start_point[0]-pix_loca[0])*(x*self.x_dist+self.bias+self.start_point[0]-pix_loca[0])
+                    n=(y*self.y_dist-self.bias+self.start_point[1]-pix_loca[1])*(y*self.y_dist+self.bias+self.start_point[1]-pix_loca[1])
+                    if m <= 0 and n <= 0 and self.statue_board[x][y] == 0: 
+                        print(player.color)                            
                         surface.blit(COLOR_PIC[player.color], 
-                                     (x+self.start_point[0] - player.bias[player.color-1][0], 
-                                      y+self.start_point[1] - player.bias[player.color-1][1]))
+                                    (x*self.x_dist+self.start_point[0] - player.bias[player.color-1][0], 
+                                    y*self.y_dist+self.start_point[1] - player.bias[player.color-1][1]))
                         pg.display.update()
+                        print(self.statue_board)
                         done =True
                         statue=[x,y]
                         continue
         return statue
         
     def start(self,surface):
+        """
+        游戏主代码
+        """
+        global done
+        winner=0
         turn=[False,False]
         turn[choice([0,1])]=True
         game=True
         while game:
-            sleep(1)
+            sleep(0.1)
             for m in range(2):
                 if turn[m]:
-                    self.place_piece(self.player[m],surface)
-                    if self.check():
-                        print("checked")
+                    turn_clear=pg.Rect(self.turn_disp_point[0],self.turn_disp_point[1],80,20) # 使用turn_clear对象覆盖原turn_statue文本
+                    pg.draw.rect(surface,(255,255,255),turn_clear)
+                    pg.display.flip()
+                    turn_statue=pg.font.SysFont("华文细黑",15).render(COLOR[self.player[m].color]+"回合",True,(0,0,0))
+                    surface.blit(turn_statue,self.turn_disp_point)
+                    pg.display.update()
+                    cache=self.place_piece(self.player[m],surface)
+                    self.statue_board[cache[0]][cache[1]]=self.player[m].color
+                    if str(cache[0]) == 'False':
+                        print('中断游戏')
+                        done=True
                         game=False
+                    if self.check():
+                        return(self.player[m].color)
                     turn.reverse()
-        self.result()
+        if not done:
+            return 0
 
-    def result(self):
-        pass
+    def result(self,winner):
+        turn_clear=pg.Rect(self.turn_disp_point[0],self.turn_disp_point[1],80,20)
+        pg.draw.rect(surface,(255,255,255),turn_clear)
+        pg.display.flip()
+        winner_text=pg.font.SysFont("华文细黑",15).render(COLOR[winner]+"获胜，右上角退出游戏",True,(0,0,0))
+        surface.blit(winner_text,self.turn_disp_point)
+        pg.display.update()
+        
 
 class player:
-    """玩家类"""
+    """
+    玩家类
+    """
     score=0
     color=0
-    bias=[[30,45],[20,30]]
+    bias=[[35,42],[25,27]] # 用于校准棋子图片显示位置偏差
     turn = False
     def __init__(self, color:int):
         self.color = color
@@ -145,16 +255,22 @@ def welcome_msg():
                     surface.fill((0,0,0))
                     pg.display.flip()
                     start=True
-    main()
 
             
 def main():
+    """
+    主程序入口
+    """
     print("main start")
     disp(surface)
     white=player(1)
     black=player(2)
     game=board(LOCA_ARGC,white,black)
-    game.start(surface)
+    res=game.start(surface)
+    if res == 0:
+        return 0
+    else:
+        game.result(res)
 
 def disp(surface):
     surface.fill((255, 255, 255))  # 设置背景颜色为白色
@@ -164,25 +280,17 @@ def disp(surface):
     pg.display.set_caption("五子棋")  # 设置窗口标题为"五子棋"
     pg.display.update()  # 更新显示
 
-
-
-"""    
-if self.color == 1:
-            surface.blit(WHITE_PIC, (location[0] - 32, location[1] - 32))
-        elif self.color == 2:
-            surface.blit(BLACK_PIC, (location[0] - 32, location[1] - 32))
-"""
+done = False
 pg.init() #pygame 初始化
 pg.font.init()  # 初始化字体模块
 surface = pg.display.set_mode((652, 652))
 disp(surface)
 welcome_msg() 
+main()
 
-done = False
+
 while not done:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             print("退出游戏")
             done = True
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            print(pg.mouse.get_pos())
